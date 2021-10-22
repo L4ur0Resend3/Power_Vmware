4a262d11863be67031211e2cd274c7fa0dd7fec1
Lauro Resende <lauro.resende@outlook.com>
2021-10-22 10:28:24 -0300

    custom atrib

---
+++ CustomAtrib.ps1
@@ -0,0 +1,22 @@
+﻿$count = 0
+cls 
+Get-VM | %{
+
+$_.Name
+#$_.Notes
+write-host $_.CustomFields
+#$_
+
+if($_.Notes -like "*"){}else{
+$Description = ""
+Set-VM -VM $_ -Notes $Description -Confirm:$false
+}
+write-host "==============================="
+#Read-Host "next.."
+$count++
+
+}
+
+$count
+#Set-CustomAttribute -Name CreationDetails -CustomAttribute CreationDate
+#New-CustomAttribute -Name "CompanyName" -TargetType VMHost, VirtualMachine
